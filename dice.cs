cdc5bdedce8565fb7fae10d3ea10a4ad115b6adf
채희찬 <eqholic1125@gmail.com>
2021-09-14 22:49:42 +0900

    구현: 주사위 굴리기 기능 구현

---
+++ dice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dice : MonoBehaviour
+{
+    public Image dice_top;
+
+    public List<Sprite> eyes = new List<Sprite>(new Sprite[6]);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void RollDice()
+    {
+        int rannum = Random.Range(0, 5);
+
+        dice_top.sprite = eyes[rannum];
+    }
+
+    public void MoveMyList()
+    {
+
+    }
+}
