6ee24db99b2acaafce7f8dbdfe8dbe82137a3b6a
채희찬 <eqholic1125@gmail.com>
2021-09-14 22:50:08 +0900

    구현: 주사위 저장 리스트 구현

---
+++ canvas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvas : MonoBehaviour
+{
+    public List<Button> MyList = new List<Button>(new Button[5]);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SortMyList()
+    {
+        if (MyList[0])
+        {
+            MyList[0].Select();
+        }
+    }
+}
